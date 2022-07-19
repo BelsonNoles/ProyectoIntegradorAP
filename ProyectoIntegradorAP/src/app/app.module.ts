fcb81b8589b2ce77321c579d55eb0431043958c0
Maximillar <villarmaximilianoariel@gmail.com>
2022-07-19 16:41:04 -0300

    Comienzo del header
    Logos AP y redes sociales

--- ProyectoIntegradorAP/src/app/app.module.ts
+++ ProyectoIntegradorAP/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesSocialesComponent,
   ],
   imports: [
     BrowserModule,
